dfcb32e9cd35e59b0e5dabe97b4aa874007d78df
jbroberto76 <jbroberto@ifce.edu.br>
2025-06-03 14:09:59 -0300

    v2025_1

---
+++ README.md
@@ -0,0 +1,12 @@
+# apihello
+
+- Código fonte utilizado na Aula 11 da disciplina Programação Web 1
+
+## Objetivo
+
+- Utilizar como exemplo de primeira implementação de uma API
+
+## Instruções
+
+1. Clonar o projeto utilizando o comando `git clone https://github.com/pw1-repo/apihello.git`
+2. Instalar as dependências com o comando `npm install`
---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+app.listen(PORT, ()=> {
+    const msg = `Hello API running on PORT ${PORT}`
+    const link = `http://localhost:${PORT}`
+    const conf = `\x1b[1m${link}\x1b[0m`
+    console.log(msg + '\n' + conf)
+})
+
+app.get('/v1/hi', function(req, res) {
+    const out = {
+        msg: "Hello, world!"
+    }
+    res.json(out)
+})
+
+app.get('/v1/hi/user/:name', function(req, res) {
+    const out = {
+        msg: "Hello, " + req.params.name
+    }
+    res.json(out)
+})
+
+app.get('*', function(req, res) {
+    const err = {
+        error: 'Invalid endpoint'
+    }
+    res.json(err)
+})
